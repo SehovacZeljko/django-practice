25841182873c12d64c6d14999da7d55da0c6af2d
Zeljko_Sehovac <zeljko.sehovac@singularity.is>
2025-07-29 16:53:19 +0200

    user todo api routes

--- todo/serializers.py
+++ todo/serializers.py
@@ -2,12 +2,31 @@ from rest_framework import serializers
 from .models import ToDoItem
 
 
+# class ToDoItemSerializer(serializers.ModelSerializer):
+#     user = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = ToDoItem
+#         fields = ['id', 'title', 'content', 'created_at', 'user']
+
+#     def get_user(self, obj):
+#         return {
+#             'id': obj.user.id,
+#             'username': obj.user.username,
+#             'email': obj.user.email,
+#             'first_name': obj.user.first_name,
+#             'last_name': obj.user.last_name
+#         }
+
+
 class ToDoItemSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField()
+    user = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = ToDoItem
         fields = ['id', 'title', 'content', 'created_at', 'user']
+        # Make user read-only since it's set automatically
+        read_only_fields = ['user', 'created_at']
 
     def get_user(self, obj):
         return {
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 from .models import ToDoItem
 from .serializers import ToDoItemSerializer
@@ -8,6 +9,19 @@ from .serializers import ToDoItemSerializer
 
 # Create your views here.
 
+# class ToDoItemViewSet(ModelViewSet):
+#     queryset = ToDoItem.objects.all()
+#     serializer_class = ToDoItemSerializer
+
+
 class ToDoItemViewSet(ModelViewSet):
-    queryset = ToDoItem.objects.all()
     serializer_class = ToDoItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Return only the current user's todo items"""
+        return ToDoItem.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        """Automatically set the user when creating a new todo item"""
+        serializer.save(user=self.request.user)
